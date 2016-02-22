83c8e920272da57f1ca8de654ded69035c37a873
PavelStryhelski <pavel.stryhelsli@gmail.com>
2016-02-22 22:02:12 +0300

    another version of singleton has been added

---
+++ Singleton/LockSingleton.cs
@@ -0,0 +1,31 @@
+﻿namespace Singleton
+{
+    public sealed class LockSingleton
+    {
+        private static volatile LockSingleton _instance;
+        private static object _syncObject = new object();
+        private LockSingleton()
+        {
+            
+        }
+
+        public static LockSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new LockSingleton();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
