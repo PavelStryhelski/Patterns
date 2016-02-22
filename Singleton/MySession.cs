de40711bd159800b16853bddd25a76a0e2b90204
PavelStryhelski <pavel.stryhelski@gmail.com>
2016-02-22 15:55:55 +0300

    singleton pattern has been added

---
+++ Singleton/MySession.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton
+{
+    sealed class MySession
+    {
+        private static MySession _session;
+        private readonly DateTime _date;
+        private readonly Dictionary<string, string> _dictionary;
+        private MySession()
+        {
+            _date = DateTime.Now;
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public static MySession Instance
+        {
+            get { return _session ?? (_session = new MySession()); }
+        }
+
+        public DateTime GetTime()
+        {
+            return _date;
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary.ContainsKey(key) ? _dictionary[key] : ""; }
+
+            set
+            {
+                if (_dictionary.ContainsKey(key))
+                {
+                    _dictionary[key] = value;
+                }
+                else
+                {
+                    _dictionary.Add(key, value);
+                }
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            MySession.Instance["user"] = "Bob Scratcher";
+            MySession.Instance["role"] = "Admin";
+
+            Console.WriteLine(MySession.Instance.GetTime());
+            Console.WriteLine(MySession.Instance["user"]);
+            Console.WriteLine(MySession.Instance["role"]);
+            Console.WriteLine(MySession.Instance.GetTime());
+            Console.WriteLine(MySession.Instance.GetTime());
+
+            Console.ReadKey();
+        }
+
+    }
+}
