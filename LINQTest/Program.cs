e170272bd9985d969cc6816f90d01eb503a6c0b8
PavelStryhelski <pavel.stryhelski@gmail.com>
2016-02-23 17:05:37 +0300

    some linq expressions have been added

---
+++ LINQTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LINQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass1.GetShortNames();
+            Console.WriteLine("\n---------------------------------------");
+            TestClass1.GetShortNamesAnotherWay();
+            Console.WriteLine("\n---------------------------------------");
+            TestClass1.GetNumbers();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQTest/TestClass1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQTest
+{
+    public class TestClass1
+    {
+        static string[] words = { "one", "two", "Bingo - black ear, white fang", "Rembo", "123", "Red Hot Chilli Peppers", "Appollo 17" };
+        static int[] numbers = { 105, 34, 28, 456, 78, 1, 15, 94, 61, 38, 14 };
+
+        public static void GetShortNames()
+        {
+            var results = from word in words
+                          where word.Length < 5
+                          select word;
+
+            PrintResults(results);
+        }
+
+        public static void GetShortNamesAnotherWay()
+        {
+            var results = words.Where(x => x.Length < 5);
+            PrintResults(results);
+        }
+
+        public static void GetNumbers()
+        {
+            var results = numbers.Where(x => x > 70);
+            PrintResults(results);
+        }
+
+        private static void PrintResults(IEnumerable array)
+        {
+            foreach (var word in array)
+            {
+                Console.Write(word + "\t");
+            }
+        }
+    }
+}
